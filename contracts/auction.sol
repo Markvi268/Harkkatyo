d96221f84f7d2e843e0cb40a219a470004a5ebce
Markvi268 <markku.viertokangas@edu.savonia.fi>
2022-11-22 22:34:57 +0200

    kohta valmis

--- contracts/auction.sol
+++ contracts/auction.sol
@@ -3,16 +3,34 @@ pragma solidity ^0.8.0;
 /* IMPORTS! */
 import "./salesannouncement.sol";
 import "hardhat/console.sol";
+
 contract Auction{
-    uint public reward; // palkkio
-    mapping(address => uint) public announcements; // ilmoitukset
-    address private owner; // huutokaupan omistaja
+
+    SalesAnnouncement sales = new SalesAnnouncement();
+    uint public reward; // palkkio %
+    mapping(uint => SalesAnnouncement) public announcements; // ilmoituksien osoite
+    address payable private owner; // huutokaupan omistaja
+    uint index = 0;
+        
 
 
     constructor(){
         reward = 5; // alustetaan välityspalkkio 5%:iin
-        owner = msg.sender;
+        owner = payable(msg.sender);
     }
+
+    receive()external payable{
+
+    }
+
+    fallback()external payable{
+
+    }
+
+    event newSalesAd(address who);
+    event rewardChange(uint reward);
+
+    
     modifier OnlyOwner(){
         require(msg.sender == owner,"Decline, you are not the owner");
         _;
@@ -20,7 +38,31 @@ contract Auction{
 
     function exchangeCommission(uint _reward)OnlyOwner public{ // vaihdetaan välityspalkkio
         reward = _reward;
+        emit rewardChange(_reward);
     }
+
+
+    function getBalance()OnlyOwner public view returns(uint){
+        return address(this).balance;
+
+    }
+
+
+    function createAnotice(string memory _title, string memory _description, uint _startprice,uint _endtime )public{
+
+        sales.newNotification(owner,payable(msg.sender),_title,_description,_startprice,_endtime,reward);
+        announcements[index] = sales;
+        emit newSalesAd(msg.sender);
+        index++;
+
+
+    }
+    function makeAshout (address _newoffer, uint amount) public {
+        sales.setYell(_newoffer, amount);
+
+    }
+
+
 
 
 }
--- contracts/salesannouncement.sol
+++ contracts/salesannouncement.sol
@@ -1,26 +1,67 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
 /* IMPORTS! */
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract SalesAnnouncement{
+
     mapping(address => uint) public huutajat;
-    string public otsikko;
-    string public kuvaus;
-    uint public pohjahinta;
-    uint public paattymisaika;
+    address payable public dealer; // huutokaupan omistaja
+    address payable public person; // ilmoituksen jättäjä
+    address payable public higherYell;
+    address payable[] public previousOffers;
+    string public title; // kohteen otsikko
+    string public description; // kohteen kuvaus
+    uint public floorprice; // pohjahinta
+    uint public endtime; // päättymisaika
+    uint public newPrice; // uusihinta
+    uint public reward;
+    uint index = 0;
 
+    function newNotification(address payable _dealer,address payable _person, string memory _title, string memory _description, uint _price, uint _endtime,uint _reward) external payable{
 
-    constructor(){
+        dealer = _dealer;
+        person = _person;
+        title = _title;
+        description = _description;
+        floorprice = _price;
+        endtime = block.number + _endtime;
+        reward = _reward;
+    }
+
+    receive() external payable{
+        require(msg.value < floorprice,"Floorprice is higher");
+        newPrice = msg.value;
+    }
+
+    event newYell(address who,uint howMuch);
+
+    modifier OnlyPerson(){
+        require(msg.sender == person,"You are not the submitter of the sale notice");
+        _;
+    }
+
+    modifier OnlyHigherShoulter(){
+        require(msg.sender == higherYell, "You are not the higher yell");
+        _;
+    }
+
+    function setYell(address _newoffer,uint offer)external payable returns(bool){
+        require(offer < floorprice,"You offer is lower than floorprice");
+        require(newPrice > offer,"You offer is too low");
+        
+        newPrice = offer;
+        emit newYell(_newoffer,offer);
+        return true;
+    }
+
+    function finished()OnlyPerson OnlyHigherShoulter public{
 
     }
 
-    function huuda () public payable {
 
-    }
 
-    function luoMyyntiIlmoitus(string memory _otsikko, string memory _kuvaus) public{
-        otsikko = _otsikko;
-        kuvaus = _kuvaus;
-    }
 
 }
